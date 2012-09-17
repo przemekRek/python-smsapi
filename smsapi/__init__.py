446517e9406f061a14e15f55bdd22ec190f4e588
Przemek Rekawiecki <przemyslaw.rekawiecki@i-dotcom.pl>
2012-09-17 16:02:22 +0200

    added method check_sms(id) to map get_sms_by_ids() smsapi method. Added  _executeSms(api_method, **params) to improve handling of response from smsapi, methods of the SmsApi class have been upgraded to handle api responce with the new handler.

--- smsapi/__init__.py
+++ smsapi/__init__.py
@@ -1,33 +1,34 @@
 #! /usr/bin/env python
-#
-# python-smsapi - Python bindings for the smsapi.pl SOAP API
-#
-# Copyright (c) 2010, Grzegorz Bialy, ELCODO.pl
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     * Neither the name of the author nor the names of its contributors may
-#       be used to endorse or promote products derived from this software
-#       without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS``AS IS'' AND ANY
-# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    #
+    # python-smsapi - Python bindings for the smsapi.pl SOAP API
+    #
+    # Copyright (c) 2010, Grzegorz Bialy, ELCODO.pl
+    # All rights reserved.
+    #
+    # Redistribution and use in source and binary forms, with or without
+    # modification, are permitted provided that the following conditions are met:
+    #     * Redistributions of source code must retain the above copyright
+    #       notice, this list of conditions and the following disclaimer.
+    #     * Redistributions in binary form must reproduce the above copyright
+    #       notice, this list of conditions and the following disclaimer in the
+    #       documentation and/or other materials provided with the distribution.
+    #     * Neither the name of the author nor the names of its contributors may
+    #       be used to endorse or promote products derived from this software
+    #       without specific prior written permission.
+    #
+    # THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS``AS IS'' AND ANY
+    # EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    # DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
+    # DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    # (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    # LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    # ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from suds.client import Client
+from suds import WebFault
 import hashlib
 import time
 
@@ -64,6 +65,29 @@ class SmsApiBaseObject(object):
         user.username = self.username
         user.password = self.password
         return user
+
+    def _executeSMSApi(self, smsApiMethod, params):
+        """Execute smsApi soup request
+        
+        in case of webfault exception there will be key 'error_webfault' 
+        present within result dictionary
+
+        """  
+
+        result = {}
+        try:
+            result.update(smsApiMethod(**params))
+        except WebFault, e:
+            result['error_webfault'] = unicode(e.fault['faultstring'])
+        finally:
+            return self._flatenResult(result)
+
+    def _flatenResult(self, result):
+        for key, value in result.items():
+            if isinstance(value, list):
+                result[key] = value[0]
+
+        return result
 
 
 class SmsApiAddressBook(SmsApiBaseObject):
@@ -148,14 +172,62 @@ class SmsApiAddressBook(SmsApiBaseObject):
 class SmsApi(SmsApiBaseObject):
 
     def get_points(self):
-        """Get user points"""
-        resp = self.client.service.get_points(self.username, self.password)
-        return "%.4f" % resp['points']
+        """Get user points
+
+        return:
+        *result type int 0 = OK lub kod bledu
+        *description type string opis bledu lub wykonanego dzialania
+        *points type float
+        """
+
+        params = {'username': self.username, 'password': self.password}
+        return self._executeSMSApi(self.client.service.get_points, params)
+
+    def check_sms(self, id):
+        """Check sent sms' by id received form send_sms(...)
+        
+        return:
+        *result type int 0 = ok lub kod bledu
+        *description type string opis bledu lub wykonanego dzialania
+        *status - optional, unbounded;  type smsstatus
+            *id type string, id wiadomosci
+            *recipient type string, numer telefonu odbiorcy
+            *sender type string, pole nadawcy
+            *message type string, tresc wiadomosci
+            *flash type int, wiadomosc flash 0/1
+            *eco type int, wiadomosc eco 0/1
+            *date_send type int, data odebrania sms'a przez serwis
+            *status type int, raport doreczenia sms'a
+            *date_submit type int, data wyslania sms'a
+            *date_done type int, data doreczenia sms'a
+        
+        exmaple: to find out status of the sms having id received 
+        with send_sms() execute:
+        check_sms(id)['status']['status']
+            
+            """
+
+        return self._executeSMSApi(self.client.service.get_sms_by_ids, {'client': self.user, 'ids': id})
 
     def send_sms(self, recipient, sender, message, eco=1, date_send=None,
             params=[], idx=None, no_unicode=0, datacoding=None,
             partner_id=None, test=0, priority=None, udh=None, flash=None):
-        """Send SMS message"""
+        """Send SMS message
+
+        return:
+        *result type int 0 = OK lub kod bledu (Dodatek nr 2)
+        *response - optional, unbounded, nillable;  type SMSIDO
+            *id type string, ID wiadomosci wewnatrz system
+            *phone type string, Numer telefonu
+            *points type float, Ilosc pobranych punktow za wysylk
+        *description type string, Opis bledu lub wykonanego dzialania
+        *count type int, Ilosc wyslanych SMS'ow
+        *points type float, Ilosc pobranych punktow za wysylke
+
+        exmaple:to find out the id of sms execute
+        send_sms(...)['response']['id']
+
+        """
         sms = self.client.factory.create('SMS')
         sms.recipient = recipient
         sms.sender = sender
@@ -178,10 +250,6 @@ class SmsApi(SmsApiBaseObject):
                 sms.date_send = time.mktime(date_send.timetuple())
             except Exception, e:
                 raise SmsApiException(e)
-        response = self.client.service.send_sms(self.user, sms)
-        if "result" in response and response["result"] == 0:
-            return {
-                'sms_id': int(response['response'][0]['id']),
-                'cost': "%.4f" % response['points'],
-            }
-        raise SmsApiException("Couldn't send SMS")
+
+        return self._executeSMSApi(self.client.service.send_sms,
+                {'client': self.user, 'sms': sms})
